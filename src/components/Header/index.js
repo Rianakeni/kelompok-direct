a74e6d3ac5e6ac71ff1a045eb766bd2162600834
Rianakeni <s22210372@student.unklab.ac.id>
2024-10-30 10:18:37 +0800

    adding Header to App.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,13 @@ import Generations from "../Generations";
 import Logo from "../Logo";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>Component Header</h1>
-            <Logo />
-            <Generations />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Component Header</h1>
+      <Logo />
+      <Generations />
+    </div>
+  );
 };
 
-export default Header
+export default Header;
