a82d6513e08c67e78c7ee2eab7c3bd8487ca62cf
Rianakeni <s22210372@student.unklab.ac.id>
2024-11-03 23:03:55 +0800

    adding footer to App js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
       <Header />
+      <Footer />
     </div>
   );
 };
