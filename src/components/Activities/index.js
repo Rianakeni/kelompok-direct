10ca4eab833eb8ab98fd6cd830776770bea5396e
Rianakeni <s22210372@student.unklab.ac.id>
2024-11-18 21:41:58 +0800

    turn activities  code to firebase

--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -1,59 +1,82 @@
 import { useState, useEffect } from "react";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { ref, get } from "firebase/database";
+import { db } from "../../Config/Firebase";
 import ActivityCard from "../ActivityCard";
+
 const Activities = () => {
-  const [images, setImages] = useState([]);
+  const [olahragaImages, setOlahraga] = useState([]);
+  const [doaSubuhImages, setDoaSubuhImages] = useState([]);
+  const [studentForumImages, setStudentForumImages] = useState([]);
+
   useEffect(() => {
-    const db = getDatabase();
     const imagesRef = ref(db, "images/");
-    onValue(imagesRef, (snapshot) => {
-      const data = snapshot.val();
-      setImages(data);
-    });
-  }, []);
-  return (
-    <main className="activities">
-      <h2 className="section-title">Our Activities</h2>
-      <ActivityCard title="Olahraga Bersama Dosen FILKOM">
-        {images.map((item) => (
-          <img
-            key={item.id}
-            src={`data:image/jpeg;base64,${item.gambar}`}
-            alt={`Activity ${item.id}`}
-          />
-        ))}
-      </ActivityCard>
-      <ActivityCard
-        title="Doa Subuh Bersama Senior"
-        images={[
-          { src: "/img/doa-subuh.jpg", alt: "Morning prayer with seniors" },
-        ]}
-      />
-      <ActivityCard
-        title="Student Forum"
-        isStudentForum={true}
-        images={[
+    get(imagesRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        const imagesData = snapshot.val();
+        setOlahraga([
           {
-            src: "img/sertifikat.jpg",
+            src: `data:image/jpeg;base64,${imagesData[1].gambar}`,
+            alt: "Morning prayer with seniors",
+          },
+        ]);
+      } else {
+        setOlahraga([]);
+      }
+    });
+    get(imagesRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        const imagesData = snapshot.val();
+        setDoaSubuhImages([
+          {
+            src: `data:image/jpeg;base64,${imagesData[2].gambar}`,
+            alt: "Morning prayer with seniors",
+          },
+        ]);
+      } else {
+        setDoaSubuhImages([]);
+      }
+    });
+
+    get(imagesRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        const imagesData = snapshot.val();
+        setStudentForumImages([
+          {
+            src: `data:image/jpeg;base64,${imagesData[3].gambar}`,
             alt: "Certificate submission",
             className: "forum-image",
           },
           {
-            src: "img/cssa1.JPG",
+            src: `data:image/jpeg;base64,${imagesData[4].gambar}`,
             alt: "Group photo",
             className: "forum-image",
           },
           {
-            src: "img/student.jpg",
+            src: `data:image/jpeg;base64,${imagesData[5].gambar}`,
             alt: "Students in classroom",
             className: "forum-image",
           },
           {
-            src: "img/cssa2.JPG",
+            src: `data:image/jpeg;base64,${imagesData[6].gambar}`,
             alt: "Large group gathering",
             className: "forum-image large",
           },
-        ]}
+        ]);
+      } else {
+        setStudentForumImages([]);
+      }
+    });
+  }, []);
+
+  return (
+    <main className="activities">
+      <h2 className="section-title">Our Activities</h2>
+      <ActivityCard title="Doa Subuh Bersama Senior" images={olahragaImages} />
+      <ActivityCard title="Doa Subuh Bersama Senior" images={doaSubuhImages} />
+      <ActivityCard
+        title="Student Forum"
+        isStudentForum={true}
+        images={studentForumImages}
       />
     </main>
   );
--- src/components/ActivityCard/index.js
+++ src/components/ActivityCard/index.js
@@ -1,19 +1,19 @@
 const ActivityCard = ({ title, images, isStudentForum }) => {
-        return (
-        <div className="activity-card">
-            <h3 className="activity-title">{title}</h3>
-            <div className={isStudentForum ? "student-forum" : "activity-images"}>
-                {images.map((img, index) => (
+  return (
+    <div className="activity-card">
+      <h3 className="activity-title">{title}</h3>
+      <div className={isStudentForum ? "student-forum" : "activity-images"}>
+        {/* {images.map((img, index) => (
                     <img 
                     key={index}
                     src={img.src} 
                     alt={img.alt} 
                     className={img.className}
                     />
-                ))}
-            </div>
-        </div>
-    );
+                ))} */}
+      </div>
+    </div>
+  );
 };
 
 export default ActivityCard;
