b13003d1877fdae18432acc78fcc38b74e91249a
Rianakeni <s22210372@student.unklab.ac.id>
2024-11-20 08:00:54 +0800

    adding tambahan firebase

--- src/components/currentMember/index.js
+++ src/components/currentMember/index.js
@@ -1,4 +1,18 @@
+import { useState, useEffect } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
+
 const CurrentMember = () => {
+  const [desc, setDesc] = useState({});
+
+  useEffect(() => {
+    const db = getDatabase();
+    const descRef = ref(db, "desc/");
+    onValue(descRef, (snapshot) => {
+      const data = snapshot.val();
+      setDesc(data);
+    });
+  }, []);
+
   return (
     <div className="container">
       <main className="content">
@@ -53,16 +67,7 @@ const CurrentMember = () => {
           />
           <div className="text">
             <h2>PARLIAMENTARY</h2>
-            <p>
-              Parliamentary atau Divisi Acara bertanggung jawab untuk
-              merencanakan dan mengorganisir acara, berkoordinasi dengan pihak
-              terkait, mengelola anggaran, serta memastikan pelaksanaan acara
-              berjalan lancar. Mereka juga harus mengatasi masalah yang muncul
-              selama acara berlangsung dan melakukan evaluasi serta menyusun
-              laporan akhir setelah acara selesai. Peran ini memastikan setiap
-              detail acara dipersiapkan dan diimplementasikan dengan baik untuk
-              mencapai tujuan yang telah ditetapkan.
-            </p>
+            <p>{desc.parlem}</p>
           </div>
         </section>
         <section className="section">
@@ -73,56 +78,28 @@ const CurrentMember = () => {
           />
           <div className="text">
             <h2>SPIRITUAL</h2>
-            <p>
-              Divisi Kerohanian bertanggung jawab untuk merencanakan dan
-              melaksanakan kegiatan keagamaan dan spiritual dalam organisasi.
-              Mereka mengoordinasikan acara seperti doa bersama, ceramah, dan
-              peringatan hari-hari besar keagamaan. Divisi ini juga menyediakan
-              dukungan moral dan spiritual bagi anggota, serta memastikan bahwa
-              nilai-nilai spiritual dan etika diterapkan dalam semua aktivitas
-              organisasi. Mereka berperan penting dalam membangun dan menjaga
-              suasana yang harmonis dan penuh makna dalam lingkungan organisasi.
-            </p>
+            <p>{desc.spiritual}</p>
           </div>
         </section>
         <section className="section reverse">
           <img src="./images/edu.jpeg" alt="Education" className="image" />
           <div className="text">
             <h2>EDUCATION</h2>
-            <p>
-              Divisi Pendidikan bertugas menyusun dan menyelenggarakan program
-              pendidikan. Divisi ini juga mengembangkan bahan ajar serta
-              memastikan program yang diselenggarakan relevan dan berkualitas.
-              Tujuannya adalah mendukung pengembangan pribadi dan profesional
-              baik mahasiswa dan anggota organisasi yang ada.
-            </p>
+            <p>{desc.education}</p>
           </div>
         </section>
         <section className="section">
           <img src="./images/PR.jpeg" alt="Public Relation" className="image" />
           <div className="text">
             <h2>PUBLIC RELATION</h2>
-            <p>
-              Divisi Public Relation bertugas membangun dan memelihara citra
-              positif organisasi melalui komunikasi dengan publik dan media.
-              Mereka mengelola hubungan dengan media. Selain itu, divisi ini
-              juga berperan dalam menangani krisis komunikasi dan memastikan
-              pesan organisasi disampaikan secara konsisten dan efektif.
-            </p>
+            <p>{desc.PR}</p>
           </div>
         </section>
         <section className="section reverse">
           <img src="./images/mulmed.jpeg" alt="Multimedia" className="image" />
           <div className="text">
             <h2>MULTIMEDIA</h2>
-            <p>
-              Divisi Multimedia bertanggung jawab untuk membuat, mengelola, dan
-              memproduksi konten visual dan audio untuk organisasi. Mereka
-              menghasilkan materi promosi seperti video, foto, dan desain grafis
-              untuk berbagai platform. Selain itu, divisi ini juga memastikan
-              bahwa semua konten multimedia mencerminkan citra dan pesan
-              organisasi secara konsisten dan menarik.
-            </p>
+            <p>{desc.mulmed}</p>
           </div>
         </section>
         <section className="section">
@@ -133,14 +110,7 @@ const CurrentMember = () => {
           />
           <div className="text">
             <h2>SPORT</h2>
-            <p>
-              Divisi Sport bertanggung jawab untuk merencanakan,
-              menyelenggarakan, dan mengawasi semua kegiatan olahraga. Mereka
-              mengatur jadwal latihan, kompetisi, dan acara olahraga lainnya,
-              serta memastikan semua kegiatan berjalan lancar dan sesuai standar
-              keselamatan. Selain itu, divisi ini juga berperan dalam
-              mempromosikan gaya hidup sehat.
-            </p>
+            <p>{desc.sport}</p>
           </div>
         </section>
         <section className="section reverse">
@@ -151,13 +121,7 @@ const CurrentMember = () => {
           />
           <div className="text">
             <h2>SENAT ANGKATAN</h2>
-            <p>
-              Divisi Perwakilan Angkatan bertugas untuk menjembatani komunikasi
-              antara mahasiswa dan fakultas. Mereka mengkoordinasikan kegiatan
-              angkatan, menyampaikan aspirasi dan feedback mahasiswa kepada
-              CSSA, serta memastikan kepentingan dan kebutuhan angkatan
-              diakomodasi dengan baik.
-            </p>
+            <p>{desc.senat}</p>
           </div>
         </section>
         <section className="advisor-section">
